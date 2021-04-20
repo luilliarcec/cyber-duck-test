8c5f130ef779fec86fcd895e3c8c695476cf511a
Luis Arce <luilliarcec@gmail.com>
2021-04-20 13:18:48 -0500

    Create Employee Factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployeeFactory extends Factory
+{
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'company_id' => Company::factory()->create(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+        ];
+    }
+}
